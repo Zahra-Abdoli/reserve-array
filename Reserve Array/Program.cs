d99ffa4fc4338b1d44e259437dadf49386fa2023
zahra1993a <33681568+zahra1993a@users.noreply.github.com>
2020-05-12 20:21:41 +0200

    Add project files.

---
+++ Reserve Array/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Reserve_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"enter your number {i + 1}:");
+                numbers[i] =  Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i=4; i >= 0; i--)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
