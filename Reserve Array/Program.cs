b8f9a4e7a30aadbbf00c7cc14b49aec4aeecf05a
zahra1993a <33681568+zahra1993a@users.noreply.github.com>
2020-05-14 23:59:15 +0200

    validation

--- Reserve Array/Program.cs
+++ Reserve Array/Program.cs
@@ -6,18 +6,23 @@ namespace Reserve_Array
     {
         static void Main(string[] args)
         {
-            int[] numbers = new int[5];
-
-            for (int i = 0; i < 5; i++)
+            try
             {
-                Console.WriteLine($"enter your number {i + 1}:");
-                numbers[i] =  Convert.ToInt32(Console.ReadLine());
-            }
+                int[] numbers = new int[5];
+
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine($"enter your number {i + 1}:");
+                    numbers[i] = Convert.ToInt32(Console.ReadLine());
+                }
+            
             for (int i=4; i >= 0; i--)
             {
                 Console.WriteLine(numbers[i]);
             }
             Console.ReadKey();
+            }
+            catch { Console.WriteLine("invalid input"); }
         }
     }
 }
